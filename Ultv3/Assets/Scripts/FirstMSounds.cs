e39bb7d6cb02b6a397fb0100b971178b5700f0e5
bdarelius246 <60321238+bdarelius246@users.noreply.github.com>
2020-12-01 12:25:41 -0500

    Sounds
    Still working on sounds

---
+++ Ultv3/Assets/Scripts/FirstMSounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstMSounds : MonoBehaviour
+{
+   public AudioSource Menu;
+
+
+
+   public void PlayMenu(){
+       Menu.Play();
+
+
+
+   }
+}
